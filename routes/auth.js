32769a76c5c275c33c72c43242002f0bca5c879b
chrlinto <fyaklekpanshak@gmail.com>
2025-07-14 14:37:32 +0100

    chore: merge local updatename

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // Register
 router.post('/register', async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const {name, email, password } = req.body;
 
     let user = await User.findOne({ email });
     if (user) return res.status(400).json({ msg: 'User already exists' });
